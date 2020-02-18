f4fea583cfe24654bfe4739e0e95f4fefff0a4c2
robertfrigerio <rhfrigerio@email.wm.edu>
2020-02-18 18:09:48 -0500

    Finished barebones implementation of Google sign-in

--- app/src/main/java/edu/cs/wm/rateeverythingatwm/GoogleSignInActivity.java
+++ app/src/main/java/edu/cs/wm/rateeverythingatwm/GoogleSignInActivity.java
@@ -1,5 +1,6 @@
 package edu.cs.wm.rateeverythingatwm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,6 +12,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -27,6 +30,8 @@ public class GoogleSignInActivity extends AppCompatActivity {
     // [END declare_auth]
 
     private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 9001;
+    private SignInButton signIn;
 
 
     @Override
@@ -34,7 +39,21 @@ public class GoogleSignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_googlesignin);
 
-        // [START config_signin]
+        signIn = findViewById(R.id.signInButton);
+        signIn.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View v){
+                                          switch (v.getId()) {
+                                              case R.id.signInButton:
+                                                  signIn();
+                                                  break;
+                                              // ...
+                                          }
+                                      }
+                                  });
+
+
+                // [START config_signin]
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -73,7 +92,7 @@ public class GoogleSignInActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d("LoginSuccess", "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-//                            updateUI(user);
+                            updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w("LoginFail", "signInWithCredential:failure", task.getException());
@@ -85,4 +104,35 @@ public class GoogleSignInActivity extends AppCompatActivity {
                 });
     }
 
-}
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("GoogleSignIn", "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+    // [START signin]
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    // [END signin]
+
+    private void updateUI(FirebaseUser user){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    }
