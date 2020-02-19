6f7fdd5e80b341e660e4fe3e3f25a57e9825aa05
WilliamKinneyWM <wdkinney@email.wm.edu>
2020-02-19 13:39:43 -0500

    Changed app design Added NearYouActivity WK

---
+++ app/src/main/java/edu/cs/wm/rateeverythingatwm/NearYouActivity.java
@@ -0,0 +1,14 @@
+package edu.cs.wm.rateeverythingatwm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NearYouActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_near_you);
+    }
+}
