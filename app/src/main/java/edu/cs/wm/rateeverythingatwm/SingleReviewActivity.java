407981394da58ab093fbde6d227ed3e27d223ca1
robertfrigerio <rhfrigerio@email.wm.edu>
2020-02-21 14:51:20 -0500

    Added SingleReviewActivity

---
+++ app/src/main/java/edu/cs/wm/rateeverythingatwm/SingleReviewActivity.java
@@ -0,0 +1,20 @@
+package edu.cs.wm.rateeverythingatwm;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.storage.FirebaseStorage;
+
+public class SingleReviewActivity extends AppCompatActivity {
+
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_review);
+    }
+
+
+}
