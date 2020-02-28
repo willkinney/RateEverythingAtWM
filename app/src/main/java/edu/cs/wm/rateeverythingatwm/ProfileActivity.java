4c936c29a037d69c84d8d3205132b6f53f1e8b3a
robertfrigerio <rhfrigerio@email.wm.edu>
2020-02-28 16:17:48 -0500

    Some changes to launcher and added profile screen

--- app/src/main/java/edu/cs/wm/rateeverythingatwm/ProfileActivity.java
+++ app/src/main/java/edu/cs/wm/rateeverythingatwm/ProfileActivity.java
@@ -1,6 +1,8 @@
 package edu.cs.wm.rateeverythingatwm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,20 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void profileOnClick(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.viewReviewsButton:
+                intent = new Intent(this, ReviewListActivity.class);
+                break;
+
+            case R.id.newReviewButton:
+                intent = new Intent(this, PostReviewActivity.class);
+                break;
+        }
+        if (intent != null)
+            startActivity(intent);
     }
 }
