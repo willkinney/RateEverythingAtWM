7c37b07303b6707f44b9751a42ad024994b1fbb5
WilliamKinneyWM <wdkinney@email.wm.edu>
2020-03-03 22:30:05 -0500

    glitchy floating action button WK

--- app/src/main/java/edu/cs/wm/rateeverythingatwm/SingleReviewActivity.java
+++ app/src/main/java/edu/cs/wm/rateeverythingatwm/SingleReviewActivity.java
@@ -23,7 +23,6 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 
 public class SingleReviewActivity extends AppCompatActivity {
 
